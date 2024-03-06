8513d5e9d4b5dadb86a0f386fb2acf676ce53d46
Mariia <mariiaboiko.it@gmail.com>
2024-03-06 20:37:38 +0200

    Remove surname property from Employee class

--- CompaniesManagment/DataAccess/Domains/Employee.cs
+++ CompaniesManagment/DataAccess/Domains/Employee.cs
@@ -3,7 +3,6 @@
     internal class Employee
     {
         public string Name { get; set; }
-        public string Surname { get; set; }
         public int Age { get; set; }
         public string Position { get; set; }
     }
